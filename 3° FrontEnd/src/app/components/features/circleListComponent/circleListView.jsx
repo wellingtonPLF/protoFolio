6793b6c1059eb16ffddb3851cd060a5be030541c
Wellignton Pessoa de Lima Filho <wellplf@gmail.com>
2023-11-21 05:29:15 -0300

    Improving Responsivity from MyServices

--- 3° FrontEnd/src/app/components/features/circleListComponent/circleListView.jsx
+++ 3° FrontEnd/src/app/components/features/circleListComponent/circleListView.jsx
@@ -2,8 +2,6 @@
 import React from 'react';
 import styles from './circleListStyle.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import jwt from './../../../../assets/images/jwt-logo.png'
-import springBoot from './../../../../assets/images/spring-boot-logo.png'
 
 const CircleListView = (props) => {
     return (
@@ -16,10 +14,13 @@ const CircleListView = (props) => {
                     ))
                 }
                 <div className={`${styles.element} ${styles.imgSkill}`} 
-                style={{backgroundImage: `url(${jwt})`, backgroundSize: "4.5vh"}}></div>
+                id={styles.jwt}></div>
+
                 <b className={styles.element} id={styles.djangoFramework}>Django</b>
+                
                 <div className={`${styles.element} ${styles.imgSkill}`} 
-                style={{backgroundImage: `url(${springBoot})`, backgroundSize: "3.5vh"}}></div>
+                id={styles.springBoot}></div>
+
                 <b className={styles.element} id={styles.netFramework}>.NET</b>
 
             </div>
